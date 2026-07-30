55287efee027e28bef10429aa2414f75f883b900
world911 <bkashs247@gmail.com>
2026-07-30 15:26:05 +0600

    Update evm.js

--- evm.js
+++ evm.js
@@ -1,316 +1,144 @@
-const RPC = {
-
-eth:"https://ethereum.publicnode.com",
-bsc:"https://bsc-dataseed.binance.org",
-polygon:"https://polygon-bor-rpc.publicnode.com",
-arb:"https://arbitrum-one.publicnode.com",
-base:"https://mainnet.base.org",
-op:"https://mainnet.optimism.io",
-
-avax:"https://api.avax.network/ext/bc/C/rpc",
-linea:"https://rpc.linea.build",
-zksync:"https://mainnet.era.zksync.io",
-scroll:"https://rpc.scroll.io",
-
-gnosis:"https://rpc.gnosischain.com",
-fantom:"https://rpc.ftm.tools",
-cronos:"https://evm.cronos.org",
-celo:"https://forno.celo.org",
-moonbeam:"https://rpc.api.moonbeam.network"
-
+const networks = {
+    ethereum: {
+        name: "Ethereum",
+        rpc: "https://ethereum-rpc.publicnode.com",
+        symbol: "ETH",
+        price: "ethereum"
+    },
+    bsc: {
+        name: "BNB Chain",
+        rpc: "https://bsc-dataseed.binance.org",
+        symbol: "BNB",
+        price: "binancecoin"
+    },
+    polygon: {
+        name: "Polygon",
+        rpc: "https://polygon-rpc.com",
+        symbol: "POL",
+        price: "matic-network"
+    },
+    arbitrum: {
+        name: "Arbitrum",
+        rpc: "https://arb1.arbitrum.io/rpc",
+        symbol: "ETH",
+        price: "ethereum"
+    },
+    optimism: {
+        name: "Optimism",
+        rpc: "https://mainnet.optimism.io",
+        symbol: "ETH",
+        price: "ethereum"
+    },
+    avalanche: {
+        name: "Avalanche",
+        rpc: "https://api.avax.network/ext/bc/C/rpc",
+        symbol: "AVAX",
+        price: "avalanche-2"
+    },
+    fantom: {
+        name: "Fantom",
+        rpc: "https://rpc.ftm.tools",
+        symbol: "FTM",
+        price: "fantom"
+    },
+    base: {
+        name: "Base",
+        rpc: "https://mainnet.base.org",
+        symbol: "ETH",
+        price: "ethereum"
+    },
+    cronos: {
+        name: "Cronos",
+        rpc: "https://evm.cronos.org",
+        symbol: "CRO",
+        price: "crypto-com-chain"
+    },
+    gnosis: {
+        name: "Gnosis",
+        rpc: "https://rpc.gnosischain.com",
+        symbol: "xDAI",
+        price: "xdai"
+    }
 };
 
 
-
-const COIN = {
-
-eth:"ethereum",
-bsc:"binancecoin",
-polygon:"matic-network",
-arb:"ethereum",
-base:"ethereum",
-op:"ethereum",
-
-avax:"avalanche-2",
-linea:"ethereum",
-zksync:"ethereum",
-scroll:"ethereum",
-
-gnosis:"xdai",
-fantom:"fantom",
-cronos:"crypto-com-chain",
-celo:"celo",
-moonbeam:"moonbeam"
-
-};
-
-
-
-let results=[];
-
-let prices={};
-
-
-
-async function rpcCall(method,params,url){
-
-let r=await fetch(url,{
-method:"POST",
-headers:{
-"Content-Type":"application/json"
-},
-body:JSON.stringify({
-
-jsonrpc:"2.0",
-id:1,
-method:method,
-params:params
-
-})
-});
-
-
-return await r.json();
-
-}
-
-
+let prices = {};
 
 
 async function loadPrices(){
 
-let ids=[...new Set(Object.values(COIN))].join(",");
+    let ids = [...new Set(
+        Object.values(networks).map(x => x.price)
+    )].join(",");
 
 
-try{
+    let res = await fetch(
+        "https://api.coingecko.com/api/v3/simple/price?ids="
+        + ids +
+        "&vs_currencies=usd"
+    );
 
-let r=await fetch(
-"https://api.coingecko.com/api/v3/simple/price?ids="+ids+"&vs_currencies=usd"
-);
+    let data = await res.json();
 
+    prices = {};
 
-let data=await r.json();
-
-
-for(let c in COIN){
-
-prices[c]=data[COIN[c]]?.usd || 0;
-
-}
-
-
-}catch(e){
-
-console.log("Price error");
-
-}
-
+    for(let id in data){
+        prices[id] = data[id].usd;
+    }
 }
 
 
 
+async function getBalance(rpc,address){
+
+    let response = await fetch(rpc,{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+            jsonrpc:"2.0",
+            method:"eth_getBalance",
+            params:[address,"latest"],
+            id:1
+        })
+    });
 
 
-function formatBalance(value){
+    let data = await response.json();
 
-return Number(value)
-.toFixed(12)
-.replace(/0+$/,"")
-.replace(/\.$/,"");
-
+    return Number(BigInt(data.result || "0")) / 1e18;
 }
 
 
 
+function formatNumber(num){
 
-async function scan(){
+    if(num === 0) return "0";
 
+    if(num < 0.000001)
+        return num.toFixed(10);
 
-results=[];
-
-
-let output=document.getElementById("result");
-
-
-output.innerHTML=
-"<tr><td colspan='6'>Scanning...</td></tr>";
-
-
-
-await loadPrices();
-
-
-
-let addresses=document
-.getElementById("addresses")
-.value
-.split("\n")
-.map(x=>x.trim())
-.filter(x=>x);
-
-
-
-let selected=document
-.getElementById("network")
-.value;
-
-
-
-let networks =
-selected==="all"
-?
-Object.keys(RPC)
-:
-[selected];
-
-
-
-for(let addr of addresses){
-
-
-for(let chain of networks){
-
-
-try{
-
-
-let balance=await rpcCall(
-"eth_getBalance",
-[addr,"latest"],
-RPC[chain]
-);
-
-
-
-let tx=await rpcCall(
-"eth_getTransactionCount",
-[addr,"latest"],
-RPC[chain]
-);
-
-
-
-let code=await rpcCall(
-"eth_getCode",
-[addr,"latest"],
-RPC[chain]
-);
-
-
-
-let amount =
-parseInt(balance.result,16)/1e18;
-
-
-
-let usd =
-amount * (prices[chain] || 0);
-
-
-
-results.push({
-
-address:addr,
-
-network:chain,
-
-balance:formatBalance(amount),
-
-usd:usd,
-
-tx:parseInt(tx.result,16),
-
-type:
-code.result==="0x"
-?
-"EOA"
-:
-"Contract"
-
-});
-
-
-
-}catch(e){
-
-console.log("Failed:",chain);
-
-}
-
-
-}
-
-
+    return Number(num.toFixed(8)).toString();
 }
 
 
 
-// highest USD first
+async function checkWallets(){
 
-results.sort((a,b)=>b.usd-a.usd);
-
-
-
-output.innerHTML="";
-
-
-
-results.forEach(r=>{
+    let addresses =
+    document.getElementById("addresses")
+    .value
+    .split("\n")
+    .map(x=>x.trim())
+    .filter(x=>x);
 
 
-output.innerHTML+=`
-
-<tr>
-
-<td>${r.address}</td>
-
-<td>${r.network}</td>
-
-<td>${r.balance}</td>
-
-<td>$${r.usd.toFixed(2)}</td>
-
-<td>${r.tx}</td>
-
-<td>${r.type}</td>
-
-</tr>
-
-`;
-
-});
+    let selected =
+    document.getElementById("network").value;
 
 
-}
-
-
-
-
-
-function downloadCSV(){
-
-
-let csv =
-"Address,Network,Balance,USD Value,TX Count,Type\n";
-
-
-results.forEach(r=>{
-
-csv+=
-`${r.address},${r.network},${r.balance},$${r.usd.toFixed(2)},${r.tx},${r.type}\n`;
-
-});
-
-
-let blob=new Blob([csv]);
-
-let a=document.createElement("a");
-
-a.href=URL.createObjectURL(blob);
-
-a.download="evm-wallets.csv";
-
-a.click();
-
-
-}
+    let networksToCheck =
+    selected === "all"
+    ? Object.keys(networks)
+    : [
